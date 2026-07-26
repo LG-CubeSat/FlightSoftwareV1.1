671fa46e4126b03200492e85d578a3058d04a71d
Wesley28w <wesleycwong@gmail.com>
2026-07-26 14:04:36 -0700

    simple circular queue

---
+++ adcs/src/commands/command.h
@@ -0,0 +1,15 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <stdint.h>
+
+// internal commands (not from obc -> mcu) don't need sync or crc as its checked by spi parser
+typedef struct
+{
+    uint8_t destination;
+    uint8_t sequence;
+    uint8_t command;
+    uint8_t lengt;
+} CommandMessage;
+
+#endif
---
+++ adcs/src/commands/command_queue.h
@@ -0,0 +1,16 @@
+#ifndef COMMAND_QUEUE_H
+#define COMMAND_QUEUE_H
+
+#include "command.h"
+
+void command_queue_initialize(void);
+
+int command_queue_push(
+    CommandMessage command
+);
+
+int command_queue_pop(
+    CommandMessage *command
+);
+
+#endif
