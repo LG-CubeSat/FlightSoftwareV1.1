f6dcb82e189069ba6c21f1b265d07ee51e4baea4
Wesley28w <wesleycwong@gmail.com>
2026-07-25 17:19:52 -0700

    starting with the adcs side

---
+++ adcs/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("LG Cubesat ADCS starting...\n");
+
+    printf("Initializing Hardware...\n");
+
+    print("Initializing SPI...\n");
+
+    print("Starting RTOS...\n");
+
+    print("ADCS ready.\n");
+
+    return 0;
+}
