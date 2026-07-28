78ee3dfc1476e03abe69398dc2b234b1fae29c67
Wesley28w <wesleycwong@gmail.com>
2026-07-28 14:16:41 -0700

    state machine

--- adcs/src/commands/command_handler.c
+++ adcs/src/commands/command_handler.c
@@ -2,13 +2,14 @@
 
 #include "command_handler.h"
 #include "../../../shared/protocol/commands.h"
+#include "../control/adcs_state.h"
 
 /* 
 Notes: 
 Uses shared commands
 Does not directly command the ADCS
 Rather it sets a state.
-IT INTERPRETS THE COMMAND
+IT INTERPRETS THE COMMAND.
 */
 
 void command_handler_initialize()
@@ -23,22 +24,39 @@ void command_handler_process(const CommandMessage *command)
     switch (command->command)
     {
         case CMD_POINT_TO_SUN:
-            printf("[COMMAND HANDLER] Request: Point to Sun");
+            printf(
+                "[COMMAND HANDLER] Request: Point to Sun.\n"
+            );
+
+            adcs_state_request(
+                ADCS_STATE_POINTING
+            );
+
             break;
         case CMD_POINT_TO_EARTH:
-            printf("[COMMAND HANDLER] Request: Point to Earth.");
+            printf("[COMMAND HANDLER] Request: Point to Earth.\n");
             break;
         case CMD_ENTER_SAFE_MODE:
-            printf("[COMMAND HANDLER] Request: Entering Safe Mode");
+            printf("[COMMAND HANDLER] Request: Enter Safe Mode.\n");
+            
+            adcs_state_request(
+                ADCS_STATE_SAFE
+            );
+
             break;
         case CMD_EXIT_SAFE_MODE:
-            printf("[COMMAND HANDLER] Request: Exiting Safe Mode");
+            printf("[COMMAND HANDLER] Request: Exit Safe Mode.\n");
+
+            adcs_state_request(
+                ADCS_STATE_IDLE
+            );
+
             break;
         case CMD_GET_ATTITUDE:
-            printf("[COMMAND HANDLER] Request: Getting Altitude");
+            printf("[COMMAND HANDLER] Request: Getting Altitude.\n");
             break;
         case CMD_GET_STATUS:
-            printf("[COMMAND HANDLER] Request: Getting Status.");
+            printf("[COMMAND HANDLER] Request: Getting Status.\n");
             break;
         default:
             printf("[COMMAND HANDLER] ERROR: Unknown Command.\n");
--- adcs/src/main.c
+++ adcs/src/main.c
@@ -5,6 +5,7 @@
 #include "commands/command_queue.h"
 #include "commands/command_task.h"
 #include "commands/command_handler.h"
+#include "control/adcs_state.h"
 
 int main(void)
 {
@@ -22,15 +23,27 @@ int main(void)
 
     command_handler_initialize();
 
+    adcs_state_initialize();
+
     printf("Starting RTOS...\n");
 
     printf("ADCS ready.\n");
 
-    uint8_t test_packet[] = 
+    uint8_t exit_safe_packet[] = 
+    {
+        0xAA, //sync
+        0x02, // Desination: ADCS
+        0x01, // Sequence
+        0x04, // exit safe mode
+        0x00, // payload length
+        0x00, // CRC
+    };
+
+    uint8_t point_to_sun_packet[] = 
     {
         0xAA, // sync
         0x02, // Destination: ADCS
-        0x01, // Sequence
+        0x02, // Sequence
         0x01, // Command: POINT_TO_SUN
         0x00, // Payload length
         0x00, // CRC (fake for now)
@@ -38,12 +51,26 @@ int main(void)
 
     printf("\nSimulating SPI reception...\n");
     
+    printf("\n Sending EXIT_SAFE_MODE...\n");
     spi_recieve(
-        test_packet,
-        sizeof(test_packet)
+        exit_safe_packet,
+        sizeof(exit_safe_packet)
     );
 
     command_task_run();
+
+    printf("\n Sending POINT_TO_SUN...\n");
+    
+    spi_recieve(
+        exit_safe_packet,
+        sizeof(exit_safe_packet)
+    );
+
+    command_task_run();
+
+    printf("\nUpdating ADCS state...\n");
+
+    adcs_state_update();
 
     return 0;
 }
