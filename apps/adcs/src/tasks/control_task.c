6079b75c3ae0619843b7845510d5a852e577dfce
Wesley28w <wesleycwong@gmail.com>
2026-08-05 15:31:14 -0700

    shell for non-queue tasks
    The only adcs tasks that need queues are telem and command I think...

--- apps/adcs/src/tasks/control_task.c
+++ apps/adcs/src/tasks/control_task.c
@@ -3,5 +3,63 @@ Control Task
 runs 20-50 hz
 Waits for attitude
 Asks Manager
-Runs correct controller, commands magnetorquers
+Runs correct controller, controls magnetorquers
 */
+
+/*
+control Task
+Continously waits for ground messages or messages from other boards
+Decodes them and updates manager
+*/
+
+#include "../../include/tasks/control_task.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "event_groups.h"
+
+// TODO.. add includes for actually integrating into other tasks
+
+#define CONTROL_TASK_PRIORITY (3)
+#define CONTROL_TASK_STACK_SIZE (1024)
+#define CONTROL_TASK_PERIOD_MS (20)
+#define CONTROL_TASK_QUEUE_LENGTH (8)
+#define CONTROL_TASK_ITEM_SIZE sizeof(uint32_t)
+
+static StackType_t xControlTaskStack[CONTROL_TASK_STACK_SIZE];
+static StaticTask_t xControlTaskBuffer;
+
+TaskHandle_t xControlHandle = NULL;
+
+void control_task_init(void)
+{
+    // initialize the task
+    xControlHandle = xTaskCreateStatic(
+        control_task,
+        "control",
+        CONTROL_TASK_STACK_SIZE,
+        NULL,
+        CONTROL_TASK_PRIORITY,
+        xControlTaskStack,
+        &xControlTaskBuffer
+    );
+}
+
+// superloop of the task
+void control_task(void *pvParameters)
+{
+    (void) pvParameters;
+
+    TickType_t lastWakeTime = xTaskGetTickCount();
+
+    for (;;)
+    {
+        // do some stuff
+
+        xTaskDelayUntil(
+            &lastWakeTime,
+            pdMS_TO_TICKS(CONTROL_TASK_PERIOD_MS)
+        );
+    }
+}
