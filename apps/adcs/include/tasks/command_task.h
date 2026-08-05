ffbff9fc870de3c6ebdf1629adeccbdf785556e2
Wesley28w <wesleycwong@gmail.com>
2026-08-05 15:43:01 -0700

    basic queue shell
    still needs some work, and also the actual command task needs to receive a custom internal command format, not csp

--- apps/adcs/include/tasks/command_task.h
+++ apps/adcs/include/tasks/command_task.h
@@ -9,9 +9,12 @@ Decodes them and updates manager
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "../../../../libs/libcsp/include/csp/csp_types.h"
 
 void command_task_init(void);
 
 void command_task(void *pvParameters);
+
+int command_task_send(csp_packet_t *command)
 
 #endif
--- apps/adcs/src/tasks/command_task.c
+++ apps/adcs/src/tasks/command_task.c
@@ -10,49 +10,92 @@ Decodes them and updates manager
 #include "task.h"
 #include "queue.h"
 #include "event_groups.h"
+#include "../../../../libs/libcsp/include/csp/csp_types.h"
 
 // TODO.. add includes for actually integrating into other tasks
 
 #define COMMAND_TASK_PRIORITY (3)
 #define COMMAND_TASK_STACK_SIZE (1024)
 #define COMMAND_TASK_PERIOD_MS (20)
+#define COMMAND_TASK_QUEUE_LENGTH (8)
+#define COMMAND_TASK_ITEM_SIZE sizeof(uint32_t)
 
-static TaskHandle_t commandTaskHandle = NULL;
-static QueueHandle_t commandQueue = NULL;
+static StackType_t xCommandTaskStack[COMMAND_TASK_STACK_SIZE];
+static StaticTask_t xCommandTaskBuffer;
+static StaticQueue_t xCommandStaticQueue;
+static uint8_t commandQueueStorageArea[COMMAND_TASK_QUEUE_LENGTH * COMMAND_TASK_ITEM_SIZE];
+
+static TaskHandle_t xCommandHandle = NULL;
+static QueueHandle_t xCommandQueue = NULL;
 
 void command_task_init(void)
 {
-    int ret; // return value checker
-    // initalize queue
-    ret = xQueueCreateStatic();
-    if (ret == 0) {
-        printf("[COMMAND_TASK] Failred to create Static Queue.\n");
+    // initalize the queue
+    xCommandQueue = xQueueCreateStatic(
+        COMMAND_TASK_QUEUE_LENGTH,
+        COMMAND_TASK_ITEM_SIZE,
+        commandQueueStorageArea,
+        &xCommandStaticQueue
+    );
+
+    if (xCommandQueue == NULL)
+    {
+        printf("[COMMAND] Queue creation failed.\n");
         return;
     }
-
-    xTaskCreateStatic(
+    
+    // initialize the task
+    xCommandHandle = xTaskCreateStatic(
         command_task,
         "Command",
         COMMAND_TASK_STACK_SIZE,
         NULL,
         COMMAND_TASK_PRIORITY,
-        &commandTaskHandle
+        xCommandTaskStack,
+        &xCommandTaskBuffer
     );
+
+    if (xCommandHandle == NULL)
+    {
+        printf("[COMMAND] Task Creation failed.\n");
+        return;
+    }
 }
 
+int command_task_send(csp_packet_t *command)
+{
+    if (xCommandQueue == NULL)
+    {
+        return 0; // failed
+    }
+
+    return xQueueSend(
+        xCommandQueue,
+        command,
+        0
+    ) == pdPASS;
+}
+
+// superloop of the task
 void command_task(void *pvParameters)
 {
     (void) pvParameters;
 
-    TickType_t lastWakeTime = xTaskGetTickCount();
+    /* 
+    TODO: instead of recieving a packet we should recieve an internal command id not packet
+    This means using a custom packet class because technically the packet should be parsed way earlier by telem
+    */
+    csp_packet_t command;
 
     for (;;)
     {
-        // do some stuff
-
-        xTaskDelayUntil(
-            &lastWakeTime,
-            pdMS_TO_TICKS(COMMAND_TASK_PERIOD_MS)
-        );
+        if (xQueueReceive(
+            xCommandQueue,
+            &command,
+            portMAX_DELAY))
+        {
+            // Decode command
+            command_process(&command); // place holder for making command actually do something
+        }
     }
 }
