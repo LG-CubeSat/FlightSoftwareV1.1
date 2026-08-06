aa57813be8d3a04d676edbf4b8e87f5abcbc4440
Wesley28w <wesleycwong@gmail.com>
2026-08-06 13:02:34 -0700

    housekeeping task shell

--- apps/adcs/src/tasks/housekeeping_task.c
+++ apps/adcs/src/tasks/housekeeping_task.c
@@ -3,3 +3,50 @@ Housekeeping Task
 Maintains basic homeostasis
 Stack usage, CPU usage, Temp, Task heartbeat
 */
+
+#include "../../include/tasks/control_task.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define HOUSEKEEPING_TASK_PRIORTIY (1)
+#define HOUSEKEEPING_TASK_STACK_SIZE (1024)
+#define HOUSEKEEPING_TASK_PERIOD_MS (20)
+
+static StackType_t xHousekeepingTaskStack[HOUSEKEEPING_TASK_STACK_SIZE];
+static StaticTask_t xHousekeepingTaskBuffer;
+
+TaskHandle_t xHousekeepingHandle = NULL;
+
+void housekeeping_task_init(void)
+{
+    xHousekeepingHandle = xTaskCreateStatic(
+        housekeeping_task,
+        "housekeeping",
+        HOUSEKEEPING_TASK_STACK_SIZE,
+        NULL,
+        HOUSEKEEPING_TASK_PRIORTIY,
+        xHousekeepingTaskStack,
+        &xHousekeepingTaskBuffer
+    );
+
+    if (xHousekeepingHandle == NULL)
+    {
+        printf("[HOUSEKEEPING] Failed to initialize.\n");
+    }
+}
+
+void housekeeping_task(void *pvParameters)
+{
+    (void)pvParameters;
+
+    TickType_t lastWakeTime = xTaskGetTickCount();
+
+    for (;;)
+    {
+        xTaskDelayUntil(
+            &lastWakeTime,
+            pdMS_TO_TICKS(HOUSEKEEPING_TASK_PERIOD_MS)
+        );
+    }
+}
