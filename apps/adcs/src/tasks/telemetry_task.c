0752d60f8a4884ed73a91372d63f3b5f0fbe1f8d
Wesley28w <wesleycwong@gmail.com>
2026-08-06 13:02:48 -0700

    telemetry task shell

--- apps/adcs/src/tasks/telemetry_task.c
+++ apps/adcs/src/tasks/telemetry_task.c
@@ -4,3 +4,51 @@ Telemetry Task
 Collects everything
 Sends out data using CSP
 */
+
+#include "../../include/tasks/telemetry_task.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define TELEMETRY_TASK_PRIORITY (3)
+#define TELEMETRY_TASK_STACK_SIZE (1024)
+#define TELEMETRY_TASK_PERIOD_MS (20)
+
+static StackType_t xTelemetryTaskStack[TELEMETRY_TASK_STACK_SIZE];
+static StaticTask_t xTelemetryTaskBuffer;
+
+TaskHandle_t xTelemetryHandle = NULL;
+
+void telemetry_task_init(void)
+{
+    xTelemetryHandle = xTaskCreateStatic(
+        telemetry_task,
+        "telemetry",
+        TELEMETRY_TASK_STACK_SIZE,
+        NULL,
+        TELEMETRY_TASK_PRIORITY,
+        xTelemetryTaskStack,
+        &xTelemetryTaskBuffer
+    );
+
+    if (xTelemetryHandle == NULL) {
+        printf("[TELEMTRY] Failed to initialize.\n");
+    }
+}
+
+void telemtry_task(void *pvParameters)
+{
+    (void) pvParameters;
+
+    TickType_t lastWakeTime = xTaskGetTickCount();
+
+    for (;;)
+    {
+        // do some stuff
+
+        xTaskDelayUntil(
+            &lastWakeTime,
+            pdMS_TO_TICKS(TELEMETRY_TASK_PERIOD_MS)
+        );
+    }
+}
