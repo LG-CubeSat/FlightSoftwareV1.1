4ff20688fcb2620c4988ad554828c6e8bb3551b7
Wesley28w <wesleycwong@gmail.com>
2026-08-20 17:13:55 -0700

    silly double print

--- apps/obc/src/main.c
+++ apps/obc/src/main.c
@@ -119,9 +119,10 @@ int main(void)
                         // NACK is 1, because it represents wire being pulled high
                         if (command_ack.status == NACK) {
                             printf("[OBC] Received NACK - Sequence: %d, Command ID: %d\n", seq, command_ack.ack_command_id);
+                        } else {
+                            printf("[OBC] Received ACK - Sequence: %d, Command ID: %d\n", seq, command_ack.ack_command_id);
                         }
 
-                        printf("[OBC] Received ACK - Sequence: %d, Command ID: %d\n", seq, command_ack.ack_command_id);
                         fflush(stdout);
                     }
                     csp_buffer_free(packet); // don't forget this whenever you use packet
