838a6d502714456f3cd88462d722ae9eec863604
Wesley28w <wesleycwong@gmail.com>
2026-08-27 16:41:53 -0700

    main init and run threads

--- apps/obc/commands/src/main.c
+++ apps/obc/commands/src/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+#include "relay.h"
+#include "ingest.h"
+
 int main(void) {
     printf("[OBC COMMAND P] Program started.\n");
 
     /*
     init stuff here
     */
+    ingest_thread_init();
+    relay_thread_init();
 
     /*
     Set off the threads
     */
+
+    ingest_thread();
+    relay_thread();
+
 
     /*
     Clean up
