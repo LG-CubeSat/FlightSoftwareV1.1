aa3a0680272021418a39c141ceec0ec81cff3b42
Wesley28w <wesleycwong@gmail.com>
2026-09-05 08:53:27 -0700

    fallback_handle_fault

--- apps/obc/fdir/include/fallback.h
+++ apps/obc/fdir/include/fallback.h
@@ -4,6 +4,18 @@
 #include "obc_supervisor_protocol.h"
 #include "obc_ipc.h"
 
-int fallback_request(supervisor_cmd_t cmd, OBC_Roles_t role);
+typedef enum {
+    UNRESPONSIVE = 1,
+    OUT_OF_BOUNDS = 2
+} fault_type_t;
 
-#endif OBC_FDIR_FALLBACK_H
+typedef struct {
+    OBC_Roles_t role;
+    fault_type_t fault_type;
+} fault_report_t;
+
+
+int fallback_request(supervisor_cmd_t cmd, OBC_Roles_t role);
+supervisor_request_t fallback_handle_fault(fault_report_t report);
+
+#endif
