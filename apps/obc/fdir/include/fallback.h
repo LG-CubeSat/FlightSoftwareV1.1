15bdbb33652cadff10fddd576705501622f94903
Wesley28w <wesleycwong@gmail.com>
2026-09-05 08:34:08 -0700

    fall back

--- apps/obc/fdir/include/fallback.h
+++ apps/obc/fdir/include/fallback.h
@@ -1,4 +1,9 @@
 #ifndef OBC_FDIR_FALLBACK_H
 #define OBC_FDIR_FALLBACK_H
 
-#endif // OBC_FDIR_FALLBACK_H
+#include "obc_supervisor_protocol.h"
+#include "obc_ipc.h"
+
+int fallback_request(supervisor_cmd_t cmd, OBC_Roles_t role);
+
+#endif OBC_FDIR_FALLBACK_H
--- apps/obc/fdir/src/fallback.c
+++ apps/obc/fdir/src/fallback.c
@@ -1,5 +1,20 @@
 #include "fallback.h"
 
+#include "obc_supervisor_protocol.h"
+#include "obc_ipc.h"
+
+#include <stdio.h>
+
 /* Actually determines how the issues will map into a new response */
 
 /* Reactive thread that responds to fallback calls. This is what queries the supervisor */
+int fallback_request(supervisor_cmd_t cmd, OBC_Roles_t role)
+{
+    supervisor_request_t req = { .cmd = (uint8_t)cmd, .role = (uint8_t)role };
+    int rc = IPC_send(ROLE_SUPERVISOR, (const uint8_t *)&req, sizeof(req));
+    if (rc < 0) {
+        fprintf(stderr, "[FDIR FALLBACK] failed to request send (cmd=%d, role=%d)\n", cmd, role);
+        return -1;
+    }
+    return 0;
+}
