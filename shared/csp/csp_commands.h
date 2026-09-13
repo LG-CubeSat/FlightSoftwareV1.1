988fa833cadf7f46f529acbba5f18fdd0b4f0ba6
Wesley28w <wesleycwong@gmail.com>
2026-09-13 16:05:22 -0700

    fixed naming scheme

--- shared/csp/csp_commands.h
+++ shared/csp/csp_commands.h
@@ -13,7 +13,7 @@ ADCS telemetry = 20).
 #define OBC_ADDRESS      1
 #define ADCS_ADDRESS     2
 #define EPS_ADDRESS      3
-#define THERMALS         4
+#define THERMALS_ADDRESS 4
 #define CAMERA           5
 #define COMMS            6
 
